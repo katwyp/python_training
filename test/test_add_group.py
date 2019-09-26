09800e73a3108ebbce795d6b8b0342c9d6f88097
katarzyna <katarzyna_wypich@parp.gov.pl>
2019-09-26 16:54:40 +0200

    Wyodrębniona klasa group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="rherhetht", header="tjejetj", footer="ryjry"))
+    app.group.create(Group(name="rherhetht", header="tjejetj", footer="ryjry"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
