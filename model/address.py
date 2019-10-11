652bec6236f90c0d15dc8924df314dad71e4580e
katwyp <k.wypich@gmail.com>
2019-10-11 12:12:52 +0200

    Zadanie 15

--- model/address.py
+++ model/address.py
@@ -23,7 +23,7 @@ class Address:
         self.id = id
 
     def __repr__(self):
-        return "%s:%s %s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s %s, %s" % (self.id, self.firstname, self.lastname, self.email)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id)\
