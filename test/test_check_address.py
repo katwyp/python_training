45a15f07565cff1821d851f7f431953900a87470
katwyp <k.wypich@gmail.com>
2019-10-11 10:00:53 +0200

    Zadanie 14 - poprawa

--- test/test_check_address.py
+++ test/test_check_address.py
@@ -2,11 +2,15 @@ import re
 from random import randrange
 
 
-def test_phones_on_home_page(app):
+def test_random_address_on_home_page(app):
     index = randrange(app.address.count())
     address_from_home_page = app.address.get_address_list()[index]
     address_from_edit_page = app.address.get_address_info_from_edit_page(index)
+    assert address_from_home_page.firstname == address_from_edit_page.firstname
+    assert address_from_home_page.lastname == address_from_edit_page.lastname
+    assert address_from_home_page.address == address_from_edit_page.address
     assert address_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(address_from_edit_page)
+    assert address_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(address_from_edit_page)
 
 
 def test_phones_on_address_view_page(app):
@@ -28,3 +32,9 @@ def merge_phones_like_on_home_page(address):
                                 filter(lambda x: x is not None,
                                        [address.homephone, address.mobile, address.workphone,
                                         address.secondaryphone]))))
+
+
+def merge_emails_like_on_home_page(address):
+    return "\n".join(filter(lambda x: x != "",
+                            filter(lambda x: x is not None,
+                                   [address.email, address.email2, address.email3])))
